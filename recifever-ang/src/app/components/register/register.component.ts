36973c929f7031104929fe6cddb8f2caa99ad413
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-02-02 07:18:45 +0100

    Added html code and component code

--- recifever-ang/src/app/components/register/register.component.ts
+++ recifever-ang/src/app/components/register/register.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -7,9 +17,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  })
+
+  matcher = new MyErrorStateMatcher();
 
   ngOnInit(): void {
+  }
+
+  onSubmit() : void {
+    if (this.registerForm.valid) {
+      alert('Logged in as: ' + this.registerForm.get('email')?.value + ' ' + this.registerForm.get('password')?.value)
+    }
   }
 
 }
