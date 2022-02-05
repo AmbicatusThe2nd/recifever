750faabd62b5b4b3effef7325f028074960d6be6
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-02-05 11:51:51 +0100

    Updated the model type

--- recefever-webapi/recefever-webapi/Models/Recipe.cs
+++ recefever-webapi/recefever-webapi/Models/Recipe.cs
@@ -1,16 +1,21 @@
-﻿namespace recefever_webapi.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace recefever_webapi.Models
 {
     public class Recipe
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         public string? Title { get; set; }
-        public string? userID { get; set; }
+        public string userID { get; set; } = null!;
         public double preperationTime { get; set; }
         public double cookingTime { get; set; }
         public int difficulty { get; set; }
         public DailyMeal dailyMeal { get; set; }
-        public string[]? photos { get; set; }
-        public IngredientModel[]? ingredients { get; set; }
-        public string[]? steps { get; set; }
+        public string[] photos { get; set; } = null!;
+        public IngredientModel[] ingredients { get; set; } = null!;
+        public string[] steps { get; set; } = null!;
     }
 }
