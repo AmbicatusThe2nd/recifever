ac1384c084e7a36112a8eb5b514144809ac21e2b
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-02-05 16:43:11 +0100

    Added recipe service

---
+++ recifever-ang/src/app/services/recipe.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders, HttpErrorResponse  } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError, catchError  } from 'rxjs';
+import { Recipe } from '../models/recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  apiUrl: string = 'https://localhost:7021/api'
+  headers: HttpHeaders = new HttpHeaders().set('Content-Type', 'application/json')
+
+  constructor(private http: HttpClient) { }
+
+  createRecipe(data: Recipe): Observable<Object> {
+    let API_URL = `${this.apiUrl}/recipe`
+    return this.http.post(API_URL, data)
+      .pipe(
+        catchError(this.error)
+      )
+  }
+
+  getAllRecipes(): Observable<Object> | Observable<Recipe[]> {
+    return this.http.get(`${this.apiUrl}/Recipe`)
+  }
+
+  getSpecificRecipe(id:string): Observable<Object> | Observable<Recipe[]> {
+    return this.http.get(`${this.apiUrl}/Recipe/${id}`)
+  }
+
+  error(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}
