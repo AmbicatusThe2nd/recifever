76a495fdfcd4392328f184de041a6f29226b03b5
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-02-12 13:00:36 +0100

    Implemented half of the form

--- recifever-ang/src/app/components/add-new/add-new.component.ts
+++ recifever-ang/src/app/components/add-new/add-new.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-add-new',
@@ -7,9 +16,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewComponent implements OnInit {
 
+  addNewRecipeForm = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.maxLength(30), Validators.minLength(3)]),
+    preparationTime: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+    coockingTime: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+    calories: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+    difficuilty: new FormControl('1-5', [Validators.required]),
+    dailyMeal: new FormControl('', [Validators.required]),
+    igredient: new FormControl('', [Validators.required, Validators.pattern(/[A-Za-z]/g)]),
+    amount: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+    measurement: new FormControl('', [Validators.required]),
+    stepTextFrom: new FormControl('', [Validators.required, Validators.pattern(/[A-Za-z]/g)])
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnSubmit(): void {
+    alert('You have submited the form')
   }
 
 }
