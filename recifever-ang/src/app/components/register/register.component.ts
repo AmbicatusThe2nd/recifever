8ab047ee6fcb9fbf2a93bb89dfcffec8aa894207
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-02-27 15:06:40 +0100

    Connected the registration with the API

--- recifever-ang/src/app/components/register/register.component.ts
+++ recifever-ang/src/app/components/register/register.component.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   registerForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
@@ -24,17 +38,32 @@ export class RegisterComponent implements OnInit {
     firstName: new FormControl('', [Validators.required, Validators.min(2)]),
     lastName: new FormControl('', [Validators.required, Validators.min(3)]),
     dateOfBirth: new FormControl('', [Validators.required]),
-  })
+  });
 
   matcher = new MyErrorStateMatcher();
 
-  ngOnInit(): void {
-  }
+  constructor(private userService: UserService, private router: Router) {}
 
-  onSubmit() : void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    alert("Zagnal sem se")
     if (this.registerForm.valid) {
-      alert('Logged in as: ' + this.registerForm.get('email')?.value + ' ' + this.registerForm.get('password')?.value)
+      alert("In tu noter")
+      const newUser: User = {
+        firstName: this.registerForm.get('firstName')?.value,
+        lastName: this.registerForm.get('lastName')?.value,
+        email: this.registerForm.get('email')?.value,
+        password: this.registerForm.get('password')?.value,
+        birthDate: this.registerForm.get('dateOfBirth')?.value,
+      };
+
+      this.userService.createUser(newUser).subscribe(
+        () => {
+          this.router.navigate(['/login']);
+        },
+        (err) => alert('Something went wrong with the server')
+      );
     }
   }
-
 }
--- recifever-ang/src/app/models/user.model.ts
+++ recifever-ang/src/app/models/user.model.ts
@@ -1,5 +1,5 @@
 export class User {
-    public Id: string | undefined
+    public Id?: string | undefined
     public firstName : string | undefined
     public lastName : string | undefined
     public email : string | undefined
