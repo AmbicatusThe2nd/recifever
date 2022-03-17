e46ee8c5a2a2b035cbda30b54f1fa862ff42f85d
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-03-17 18:59:47 +0100

    Added new Models

---
+++ recifever_blz/recifever_blz/Models/IngredientModel.cs
@@ -0,0 +1,9 @@
+﻿namespace recifever_blz.Models
+{
+    public class IngredientModel
+    {
+        public string? Ingredient { get; set; }
+        public double Amount { get; set; }
+        public string? Measurement { get; set; }
+    }
+}
---
+++ recifever_blz/recifever_blz/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿namespace recifever_blz.Models
+{
+    public class Recipe
+    {
+        public string? Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public double PreperationTime { get; set; }
+        public double Calories { get; set; }
+        public int Difficulty { get; set; }
+        public int DailyMeal { get; set; }
+        public string[]? Photos { get; set; }
+        public IngredientModel Ingredients { get; set; } = null!;
+        public string[] Steps { get; set; } = null!;
+    }
+}
