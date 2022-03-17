21b50d7682a5bd311ac0b306a99a4279347d771e
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-03-17 19:09:57 +0100

    Added recipe form model

---
+++ recifever_blz/recifever_blz/Models/RecipeFormModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace recifever_blz.Models
+{
+    public class RecipeFormModel
+    {
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Title")]
+        public string Title { get; set; } = null!;
+        [Required(ErrorMessage = "This field is required")]
+        [Range(0.0, Double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
+        [Display(Name = "Title")]
+        public double PreperationTime { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [Range(0.0, Double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
+        [Display(Name = "Title")]
+        public double Calories { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Title")]
+        [Range(1, 5, ErrorMessage = "Please stay in range 1-5")]
+        public int Difficulty { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Title")]
+        [Range(1, 5, ErrorMessage = "Please stay in range 1-5")]
+        public int DailyMeal { get; set; }
+
+        // public string[]? Photos { get; set; } This will be implemented later
+        public IngredientModel Ingredients { get; set; } = null!;
+        public string[] Steps { get; set; } = null!;
+    }
+}
