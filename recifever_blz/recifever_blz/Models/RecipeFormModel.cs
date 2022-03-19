8603dcfa7ca241f0fdc1873dfe7ed72820f5d180
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-03-19 10:09:33 +0100

    added missing properties

--- recifever_blz/recifever_blz/Models/RecipeFormModel.cs
+++ recifever_blz/recifever_blz/Models/RecipeFormModel.cs
@@ -9,18 +9,22 @@ namespace recifever_blz.Models
         public string Title { get; set; } = null!;
         [Required(ErrorMessage = "This field is required")]
         [Range(0.0, Double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
-        [Display(Name = "Title")]
+        [Display(Name = "Preperation Time")]
         public double PreperationTime { get; set; }
         [Required(ErrorMessage = "This field is required")]
         [Range(0.0, Double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
-        [Display(Name = "Title")]
+        [Display(Name = "Cooking Time")]
+        public double CookingTime { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [Range(0.0, Double.MaxValue, ErrorMessage = "The field {0} must be greater than {1}.")]
+        [Display(Name = "Calories")]
         public double Calories { get; set; }
         [Required(ErrorMessage = "This field is required")]
-        [Display(Name = "Title")]
+        [Display(Name = "Difficulty")]
         [Range(1, 5, ErrorMessage = "Please stay in range 1-5")]
         public int Difficulty { get; set; }
         [Required(ErrorMessage = "This field is required")]
-        [Display(Name = "Title")]
+        [Display(Name = "DailyMeal")]
         [Range(1, 5, ErrorMessage = "Please stay in range 1-5")]
         public int DailyMeal { get; set; }
 
