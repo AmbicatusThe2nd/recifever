5d811e838fbbb4a2c2594eb2cbe1d144cf7d70c9
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-03-26 09:58:59 +0100

    Added user service

---
+++ recifever_blz/recifever_blz/Services/AuthServices/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using recifever_blz.Models;
+using System.Net;
+
+namespace recifever_blz.Services.AuthServices
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetUserById(string id);
+        Task<HttpStatusCode> CreateUser(RegistrationModel newUser);
+    }
+}
---
+++ recifever_blz/recifever_blz/Services/AuthServices/UserService.cs
@@ -0,0 +1,35 @@
+﻿using recifever_blz.Models;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace recifever_blz.Services.AuthServices
+{
+    public class UserService : IUserService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public UserService(HttpClient httpClient, IConfiguration configuration)
+        {
+            this._httpClient = httpClient;
+            this._configuration = configuration;
+        }
+
+        public async Task<HttpStatusCode> CreateUser(RegistrationModel newUser)
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync(_configuration["APIs:recifeverAPIUrl"] + "/api/User/"
+                , newUser);
+            return response.StatusCode;
+        }
+
+        public async Task<IEnumerable<User>> GetAll() // This might get deleted or implemented later
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<User>>(_configuration["APIs:recifeverAPIUrl"] + "/api/User/" );
+        }
+
+        public async Task<User> GetUserById(string id)
+        {
+            return await _httpClient.GetFromJsonAsync<User>(_configuration["APIs:recifeverAPIUrl"] + "/api/User/" + id);
+        }
+    }
+}
