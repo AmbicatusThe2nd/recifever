1ab48e3616319170952fe0f6acab002008b5df46
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-04-10 15:19:54 +0200

    Added component to route and added navbar

--- recifever-rct/src/App.js
+++ recifever-rct/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Add_new from './components/Add_new/Add_new';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Routes>
           <Route exact path="/" element={
             <Home />
+          } />
+          <Route exact path="/add-new" element={
+            <Add_new />
           } />
           <Route exact path="/login" element={
             <Login />
--- recifever-rct/src/components/Add_new/Add_new.js
+++ recifever-rct/src/components/Add_new/Add_new.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
 import styles from './Add_new.module.css';
 
+
 const Add_new = () => (
-  <div className={styles.Add_new} data-testid="Add_new">
-    Add_new Component
-  </div>
+  <>
+    <Navbar />
+    <div className='component-content'>
+      <div className={styles.Add_new} data-testid="Add_new">
+        Add_new Component
+      </div>
+    </div>
+  </>
 );
 
 Add_new.propTypes = {};
