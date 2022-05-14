fed739d9271aaef134c69c579d999b039c0bfe21
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-05-14 09:55:25 +0200

    Added user service

---
+++ recifever-rct/src/services/UserService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export function getAllUsers() {
+    // TODO: Implement
+}
+
+export function getSpecificUser(id) {
+    // TODO: Implement
+}
+
+export function addUser(userDetails) {
+    // TODO: Implement
+}
+
+export function updateUser(userDetails) {
+    // TODO: Implement
+}
+
+export function deleteUser(id) {
+    // TODO: Implement
+}
