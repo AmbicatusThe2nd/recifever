09a21db51ed35fe6e0ff6d94675818a097588d66
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2022-05-14 10:26:48 +0200

    Update Home.js

--- recifever-rct/src/components/Home/Home.js
+++ recifever-rct/src/components/Home/Home.js
@@ -5,73 +5,75 @@ import { Grid } from '@mui/material';
 import CustomCard from '../CustomCard/CustomCard';
 import { RecipeService } from '../../services/RecipeService';
 
-const Home = () => (
-  <>
-    <Navbar />
-    <div className='home-content'>
-      <div className={styles.Home} data-testid="Home">
-        <Grid container>
-          <Grid item>
-            <CustomCard  
-              Title='NK Maribor' 
-              Summery='Established in 1960, it growed to be the best club in Slovenia'
-              ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className='home-content'>
+        <div className={styles.Home} data-testid="Home">
+          <Grid container>
+            <Grid item>
+              <CustomCard
+                Title='NK Maribor'
+                Summery='Established in 1960, it growed to be the best club in Slovenia'
+                ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
               />
-          </Grid>
-          <Grid item>
-            <CustomCard  
-              Title='NK Maribor' 
-              Summery='Established in 1960, it growed to be the best club in Slovenia'
-              ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
+            </Grid>
+            <Grid item>
+              <CustomCard
+                Title='NK Maribor'
+                Summery='Established in 1960, it growed to be the best club in Slovenia'
+                ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
               />
-          </Grid>
-          <Grid item>
-            <CustomCard  
-              Title='NK Maribor' 
-              Summery='Established in 1960, it growed to be the best club in Slovenia'
-              ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
+            </Grid>
+            <Grid item>
+              <CustomCard
+                Title='NK Maribor'
+                Summery='Established in 1960, it growed to be the best club in Slovenia'
+                ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
               />
-          </Grid>
-          <Grid item>
-            <CustomCard  
-              Title='NK Maribor' 
-              Summery='Established in 1960, it growed to be the best club in Slovenia'
-              ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
+            </Grid>
+            <Grid item>
+              <CustomCard
+                Title='NK Maribor'
+                Summery='Established in 1960, it growed to be the best club in Slovenia'
+                ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
               />
-          </Grid>
-          <Grid item>
-            <CustomCard  
-              Title='NK Maribor' 
-              Summery='Established in 1960, it growed to be the best club in Slovenia'
-              ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
+            </Grid>
+            <Grid item>
+              <CustomCard
+                Title='NK Maribor'
+                Summery='Established in 1960, it growed to be the best club in Slovenia'
+                ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
               />
-          </Grid>
-          <Grid item>
-            <CustomCard  
-              Title='NK Maribor' 
-              Summery='Established in 1960, it growed to be the best club in Slovenia'
-              ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
+            </Grid>
+            <Grid item>
+              <CustomCard
+                Title='NK Maribor'
+                Summery='Established in 1960, it growed to be the best club in Slovenia'
+                ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
               />
-          </Grid>
-          <Grid item>
-            <CustomCard  
-              Title='NK Maribor' 
-              Summery='Established in 1960, it growed to be the best club in Slovenia'
-              ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
+            </Grid>
+            <Grid item>
+              <CustomCard
+                Title='NK Maribor'
+                Summery='Established in 1960, it growed to be the best club in Slovenia'
+                ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
               />
-          </Grid>
-          <Grid item>
-            <CustomCard  
-              Title='NK Maribor' 
-              Summery='Established in 1960, it growed to be the best club in Slovenia'
-              ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
+            </Grid>
+            <Grid item>
+              <CustomCard
+                Title='NK Maribor'
+                Summery='Established in 1960, it growed to be the best club in Slovenia'
+                ImagePath='https://www.nkmaribor.com/Img/Novice/0910//DSC_2812_tonemapped_4.jpg'
               />
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  )
+};
 
 Home.propTypes = {};
 
